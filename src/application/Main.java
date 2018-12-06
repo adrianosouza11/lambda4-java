39083d5da0854e02bc2f81aeda662b856d476b69
Adriano Souza <adrianosouza11@gmail.com>
2018-12-06 00:16:15 -0200

    Solution with expression lambda declared

--- src/application/Main.java
+++ src/application/Main.java
@@ -6,6 +6,7 @@ import util.UpperCaseName;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 public class Main {
@@ -19,7 +20,9 @@ public class Main {
         list.add(new Product("Tablet", 350.50));
         list.add(new Product("HD Case", 80.90));
 
-        List<String> names =  list.stream().map(Product::nonstaticUpperCaseName).collect(Collectors.toList());
+        Function<Product, String> prod = p -> p.getName().toUpperCase();
+
+        List<String> names =  list.stream().map(prod).collect(Collectors.toList());
 
         names.forEach(System.out::println);
     }
